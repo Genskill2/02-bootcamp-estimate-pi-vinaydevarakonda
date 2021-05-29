9ebf80ca0fe7f7980692e8221efeb3e50b939891
vinaydevarakonda <68640702+vinaydevarakonda@users.noreply.github.com>
2021-05-29 20:50:00 +0530

    Update estimate.py

--- estimate.py
+++ estimate.py
@@ -1,5 +1,6 @@
 import math
 import unittest
+import random
 
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
@@ -36,3 +37,16 @@ def wallis(iterations):
     n = 4*(i**2)
     pi = pi * n/(n-1)
   return pi*2
+
+def monte_carlo(iterations):
+
+  incircle = 0
+  for i in range(iterations):
+    x = random.random()
+    y = random.random()
+    r = ((x**2)+(y**2))**0.5 
+    
+    if r<=1:
+      inircle = incircle+1
+  pi = 4*(incircle/iterations)
+  return pi
